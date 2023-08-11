10fb2ea3e5c6195b0179eac5ae68a7063b485395
Sonalika <sonalika011@gmail.com>
2023-08-12 01:53:59 +0530

    First commit

---
+++ src/Calculator.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+
+const Calculator = () => {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name))
+  }
+  const clear = () => {
+    setResult("")
+  }
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1))//or simply slice(0, -1)
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString())
+    }
+    catch (err) {
+      setResult("error")
+    }
+
+  }
+  return (
+    <>
+      <div className="container">
+        <input type="text" value={result} />
+        <div className="keypad">
+          <button id="clear" onClick={clear}>Clear</button>
+          <button id="backspace" onClick={backspace}>C</button>
+          <button name="/" onClick={handleClick}>&divide;</button>
+          <button name="7" onClick={handleClick}>7</button>
+          <button name="8" onClick={handleClick}>8</button>
+          <button name="9" onClick={handleClick}>9</button>
+          <button name="*" onClick={handleClick}>&times;</button>
+          <button name="4" onClick={handleClick}>4</button>
+          <button name="5" onClick={handleClick}>5</button>
+          <button name="6" onClick={handleClick}>6</button>
+          <button name="-" onClick={handleClick}>&ndash;</button>
+          <button name="1" onClick={handleClick}>1</button>
+          <button name="2" onClick={handleClick}>2</button>
+          <button name="3" onClick={handleClick}>3</button>
+          <button name="+" onClick={handleClick}>+</button>
+          <button name="0" onClick={handleClick}>0</button>
+          <button name="." onClick={handleClick}>.</button>
+          <button id="result" onClick={calculate}>=</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Calculator;
